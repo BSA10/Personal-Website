4baf10de441f1bb05b9cbe8ad2873e527a3dcc78
BASSAM <sold@outlook.com>
2022-07-04 13:14:36 +0300

    Personal Website V2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,25 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AdminComponent } from './admin/admin.component';
-import { AuthGuardService } from './auth-guard.service';
-import { EditProfileComponent } from './auth/edit-profile/edit-profile.component';
-import { LoginComponent } from './auth/login/login.component';
-import { ProfileComponent } from './auth/profile/profile.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { AddPostComponent } from './blog/add-post/add-post.component';
-import { BlogComponent } from './blog/blog.component';
-import { SingPostComponent } from './blog/sing-post/sing-post.component';
-import { IndexComponent } from './index/index.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ArchiveComponent } from './components/archive/archive.component';
 
 const routes: Routes = [
-  {path:'',component:IndexComponent},
-  {path:'admin',component:AdminComponent, canActivate: [AuthGuardService]},
-  {path:'blog',component:BlogComponent },
-  {path:'blog/add-post',component:AddPostComponent , canActivate: [AuthGuardService]},
-  {path:'post/:id', component: SingPostComponent},
-  {path:'profile',component: ProfileComponent},
-  {path:'edit-profile',component: EditProfileComponent},
-  {path:'edit-profile/:password',component: EditProfileComponent},
-  {path:'login' , component:LoginComponent},
-  {path:'signup' , component:RegisterComponent},
-  {path:'**',redirectTo:'',pathMatch:'full'}
+
+  {path: ':language?',      component: HomeComponent},
+  {path: ':language?/proyectos',   component: ArchiveComponent},
+  
+  // {path: 'profile'        ,   component: ProfileComponent         , canActivate: [AuthGuard]},
+  // {path: 'users'          ,   component: UsersComponent           , canActivate: [AuthGuard]},
+  // {path: 'register-user'  ,   component: RegisterUserComponent    , canActivate: [AuthGuard]},
+
+  {path: '**', pathMatch: 'full', redirectTo: '/'},
+
 ];
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,53 +1,49 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { LoginComponent } from './auth/login/login.component';
-import { HeaderComponent } from './index/header/header.component';
-import { IndexComponent } from './index/index.component';
-import { MainComponent } from './index/main/main.component';
-import { FooterComponent } from './index/footer/footer.component';
-import { BlogComponent } from './blog/blog.component';
-import { AddPostComponent } from './blog/add-post/add-post.component';
-import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientInterceptor } from './http-client-interceptor';
-import { EditorModule } from '@tinymce/tinymce-angular';
-import { SingPostComponent } from './blog/sing-post/sing-post.component';
-import { AdminComponent } from './admin/admin.component';
-import { PdfViewerModule } from 'ng2-pdf-viewer';
-import { AdminHeaderComponent } from './admin/admin-header/admin-header.component';
-import { ProfileComponent } from './auth/profile/profile.component';
-import { EditProfileComponent } from './auth/edit-profile/edit-profile.component';
+import { HomeModule } from './components/home/home.module';
+import { GeneralModule } from './components/general/general.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { NgxGoogleAnalyticsModule } from 'ngx-google-analytics';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+
+export function HttpLoaderFactory(http: HttpClient){
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    LoginComponent,
-    HeaderComponent,
-    IndexComponent,
-    MainComponent,
-    FooterComponent,
-    BlogComponent,
-    AddPostComponent,
-    SingPostComponent,
-    AdminComponent,
-    AdminHeaderComponent,
-    ProfileComponent,
-    EditProfileComponent,
+    /* ArchiveComponent */
   ],
   imports: [
+    BrowserAnimationsModule,
+
+    HomeModule,
+    GeneralModule,
+
+    AnimateOnScrollModule.forRoot(),
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    EditorModule,
-    PdfViewerModule
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    NgxGoogleAnalyticsModule.forRoot(environment.trackAnalyticID),
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+    })
   ],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass: HttpClientInterceptor, multi: true}],
+  providers: [TranslateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
